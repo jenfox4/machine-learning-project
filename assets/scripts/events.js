6066db0db91e82968df88dd1a24c1eceff1c21a0
Jen Fox <jenniferfox4@gmail.com>
2018-10-02 13:51:55 -0400

    Events.js now calculates percentage of rgb to put into neural network User can import colors and returns percentage

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,6 @@
 const getFormFields = require('../../lib/get-form-fields.js')
+const makePrediction = require('./machine-learning/brain.js')
+const calculate = require('./machine-learning/calculate_rgb.js')
 
 const showPrediction = function () {
   console.log('show prediction')
@@ -15,7 +17,15 @@ const showTraining = function () {
 const predict = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
-  console.log(data)
+  console.log('data', data)
+  console.log('red', data.color.red)
+  const red = data.color.red
+  console.log('green', data.color.green)
+  const green = data.color.green
+  console.log('blue', data.color.blue)
+  const blue = data.color.blue
+  const computerReadable = calculate(red, green, blue)
+  console.log(computerReadable)
 }
 module.exports = {
   showPrediction,
